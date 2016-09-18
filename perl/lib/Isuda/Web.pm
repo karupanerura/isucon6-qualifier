a1538e49b36bf5b8e954d54975ca04f79dcd74ed
ar_tama <arata.makoto@gmail.com>
2016-09-18 14:51:56 +0900

    Select htmlified cache

--- perl/lib/Isuda/Web.pm
+++ perl/lib/Isuda/Web.pm
@@ -47,6 +47,7 @@ my $cache = Cache::Memcached::Fast::Safe->new({
 }
 
 my $CACHE_KEY_KEYWORDS = 'keywords';
+my $CACHE_KEY_HTML     = 'html';
 
 sub config {
     state $conf = {
@@ -140,7 +141,7 @@ get '/' => [qw/set_name/] => sub {
         push @entries, grep { $_->{id} == $id } @$entries;
     }
     for my $entry (@entries) {
-        $entry->{html}  = $self->htmlify($c, $entry->{description});
+        $entry->{html}  = $self->htmlify($c, $entry->{id}, $entry->{description});
         $entry->{stars} = $self->load_stars($entry->{keyword});
     }
 
@@ -276,6 +277,15 @@ post '/keyword/:keyword' => [qw/set_name authenticate/] => sub {
 };
 
 sub htmlify {
+    my ($self, $c, $id, $content) = @_;
+    my $cache_key = $CACHE_KEY_HTML . ":$id";
+    $cache->get_or_set(
+        $cache_key,
+        sub { $self->_htmlify($c, $content) }
+    );
+}
+
+sub _htmlify {
     my ($self, $c, $content) = @_;
     return '' unless defined $content;
     my $keywords = $self->_get_sorted_keywords;
